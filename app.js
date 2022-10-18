7b582b40575c48d32bd3ab80eafa91caed1235d9
Vitor Fel <vitorfel2000@gmail.com>
2022-10-18 20:41:29 -0300

    :iphone: Móvel de layout de páginas

---
+++ app.js
@@ -0,0 +1,11 @@
+function registerExpense() {
+    let year = document.getElementById('year');
+    let month = document.getElementById('month').value;
+    let day = document.getElementById('day');
+    let type = document.getElementById('type');
+    let description = document.getElementById('description');
+    let value = document.getElementById('value');
+
+    console.log(year.value);
+    console.log(month);
+}
